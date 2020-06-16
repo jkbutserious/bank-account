23278db520796efc3e31b8a5a2978cce87d87dd9
Kate Skorija <kateskorija@gmail.com>
2020-06-16 16:31:41 -0500

    add business and user interface logic
    > Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,45 @@ function BankAccount (accountName, accountBalance) {
 };
 
 BankAccount.prototype.addAccount = function(userName, balance) {
-  this.acccountName.push(userName);
-  this.accountBalance.push(balance);
+  this.accountName = userName;
+  this.accountBalance = balance;
 }
 
 BankAccount.prototype.balanceChange = function(balance) {
-  this.accountBalance.push(balance);
+  this.accountBalance = balance;
 }
 
 
 // User Interface Logic --------------------------
 
+$(document).ready(function() {
+  let bankAccount = new BankAccount();
+  $("form#register").submit(function(event) {
+    event.preventDefault();
+
+    const inputtedName = $("input#userName").val();
+    const inputtedBalance = parseInt($("input#initialDeposit").val());
+    bankAccount.addAccount(inputtedName, parseInt(inputtedBalance));
+
+    $("input#userName").val("");
+    $("input#initialDeposit").val("0");
+
+    $("#currentBalance").text("$" + bankAccount.accountBalance);
+
+  });
+
+  $("form#updateBalance").submit(function(event) {
+    event.preventDefault();
+
+    const inputtedDeposit = parseInt($("input#depositAmount").val());
+    const inputtedWithdrawal = parseInt($("input#withdrawalAmount").val());
+    const updatedBalance = bankAccount.accountBalance + inputtedDeposit - inputtedWithdrawal
+    bankAccount.balanceChange(updatedBalance);
+
+    $("input#depositAmount").val("0");
+    $("input#withdrawalAmount").val("0");
+
+    $("#currentBalance").text("$" + bankAccount.accountBalance);
+
+  });
+});
